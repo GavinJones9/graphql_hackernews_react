cc0af6c92121d436257a807942b51da5a12a52f1
Gavin <jog82837@gmail.com>
2022-09-04 01:39:53 -0400

    initial commit

---
+++ src/components/Link.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { AUTH_TOKEN } from '../constants';
+import {timeDifferenceForDate} from './utils';
+import { useMutation } from '@apollo/client/react';
+import {  gql } from '@apollo/client';
+
+const VOTE_MUTATION = gql`
+  mutation VoteMutation($linkId: ID!) {
+    vote(linkId: $linkId) {
+      id
+      link {
+        id
+        votes {
+          id
+          user {
+            id
+          }
+        }
+      }
+      user {
+        id
+      }
+    }
+  }
+`;
+const Link = (props) => {
+    const { link } = props;
+    const [vote] = useMutation(VOTE_MUTATION, {
+        variables: {
+          linkId: link.id
+        }
+      });
+    const authToken = localStorage.getItem(AUTH_TOKEN);
+    return (
+        <div className="flex mt2 items-start">
+            <div className="flex items-center">
+                <span className="gray">{props.index + 1}.</span>
+                {authToken && (
+                    <div
+                        className="ml1 gray f11"
+                        style={{ cursor: 'pointer' }}
+                        onClick={vote}
+                        >
+                        ▲
+                    </div>
+                )}
+            </div>
+            <div className="ml1">
+                <div>
+                    {link.description} ({link.url})
+                </div>
+                {(
+                    <div className="f6 lh-copy gray">
+                        {link.votes.length} votes | by{' '}
+                        {link.postedBy ? link.postedBy.name : 'Unknown'}{' '}
+                        {timeDifferenceForDate(link.createdAt)}
+                    </div>
+                )}
+            </div>
+        </div>
+    );
+};
+
+export default Link;
